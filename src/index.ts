e3f1005dfe241768345f8d9d8bb2a0b2a83c1f77
luxass <lucasnrgaard@gmail.com>
2023-09-27 19:04:57 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export interface Env {
+  GITHUB_TOKEN: string
+}
+
+export default {
+  async fetch(
+    request,
+    env,
+    ctx,
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    return new Response(`Hello ${url.searchParams.get('name') || 'World'}!`);
+  },
+} satisfies ExportedHandler<Env>;
