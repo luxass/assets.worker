7a61deac413f833bd5191a7432ff5feb5ff4a4dc
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-06-16 11:51:25 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,9 @@ app.get("/view-source", (c) => {
 });
 
 app.get("/ping", (c) => {
-  return c.text("pong");
-});
+  c.status(418)
+  return c.text("pong!");
+})
 
 app.get(
   "/api/fonts/*",
@@ -106,30 +107,32 @@ app.use(async (c) => {
 
 app.onError(async (err, c) => {
   console.error(err);
+  const url = new URL(c.req.url);
   if (err instanceof HTTPException) {
     return c.json({
-      path: c.req.url,
+      path: url.pathname,
       status: err.status,
       message: err.message,
       timestamp: new Date().toISOString(),
-    });
+    })
   }
 
   return c.json({
-    path: c.req.url,
+    path: url.pathname,
     status: 500,
     message: "Internal server error",
     timestamp: new Date().toISOString(),
-  });
+  })
 });
 
 app.notFound(async (c) => {
+  const url = new URL(c.req.url);
   return c.json({
-    path: c.req.url,
+    path: url.pathname,
     status: 404,
     message: "Not found",
     timestamp: new Date().toISOString(),
-  });
+  })
 });
 
 export default app;
